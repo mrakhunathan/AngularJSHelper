3e737bc978f2c6be1dfe04a0e9fa738e701a477e
Rakhunathan.M <mrakhunathan@gmail.com>
2015-09-14 19:42:30 +0530

    EOD

--- WebContent/services/MathService/app.js
+++ WebContent/services/MathService/app.js
@@ -1,5 +1,9 @@
-var mathApp = angular.module('MathApplication',['MathServiceModule']);
-mathApp.controller('mathController',['MathService',function(MathService)
-		{
-			console.log(MathService.add(2,2));
-		}]);
+var mathApp = angular.module('MathApplication', [ 'MathServiceModule' ]);
+mathApp.controller('mathController', [ '$scope','Calculator','MathService',
+		function($scope, Calculator, MathService) {
+			console.log(MathService.add(5, 2));
+			console.log(MathService.subract(5, 2));
+			console.log(MathService.multiply(5, 2));
+			console.log(MathService.divide(5, 2));
+			console.log(MathService.modulo(5, 2));
+		} ]);
