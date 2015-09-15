f5fc5d78c243a2c3b0d74e798d2be1ec1509b8c8
Rakhunathan.M <mrakhunathan@gmail.com>
2015-09-15 14:04:55 +0530

    Factory and Services

--- WebContent/twowaydatawithservices/MathService/calculate.js
+++ WebContent/twowaydatawithservices/MathService/calculate.js
@@ -1,11 +1,8 @@
-/*mathApp.service('Calculators',['$scope',function($scope)*/
 mathApp.service('Calculators',[function()
 {	
-	//$scope.addResult = "sss";
 	console.log("Inside Calculator Service..");
 	this.calculate = function()
 	{
-		//$scope.addResult = "hello";
 		console.log("Inside Calculate Service Method..");
 	}
 }]);
